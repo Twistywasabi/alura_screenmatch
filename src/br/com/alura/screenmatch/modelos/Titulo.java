4e7c72d407666fb84ecca26162eb9b5031a3b421
TwistyWasabi17_LG <lucas-hideaki@hotmail.com>
2024-09-27 10:05:36 -0300

    Use of GSON to Serialize a list of movies

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,9 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Titulo implements Comparable<Titulo>{
+import br.com.alura.screenmatch.excecao.ErroConversaoDeAnoException;
+import com.google.gson.annotations.SerializedName;
 
+public class Titulo implements Comparable<Titulo>{
     private String nome;
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
@@ -12,6 +14,16 @@ public class Titulo implements Comparable<Titulo>{
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+
+        if(meuTituloOmdb.year().length() > 4) {
+            throw new ErroConversaoDeAnoException("Não consegui converter o ano pois tem mais de 4 caracteres");
+        }
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
     }
 
     public String getNome() {
@@ -71,4 +83,12 @@ public class Titulo implements Comparable<Titulo>{
 
         return this.getNome().compareTo(outroTitulo.getNome());
     }
+
+    @Override
+    public String toString() {
+        return "(nome = " + nome +
+                ", anoDeLancamento = " + anoDeLancamento + ","
+                + "duração = " + duracaoEmMinutos + ")";
+    }
+
 }
